ba8597eb1261b6ae30e2f1d5dfffa9dd423e4ec9
Harish1982-git <mannepalli.qa@gmail.com>
2021-05-15 06:12:40 +0000

    First Commit my-devopsapp

---
+++ my-devopsapp/src/main/java/com/Harish/app/App.java
@@ -0,0 +1,13 @@
+package com.Harish.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
